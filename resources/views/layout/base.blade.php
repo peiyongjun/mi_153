2467144b397bfcf7e696287cd9af0fc6a15bedae
xmm <348427158@qq.com>
2016-10-08 19:46:47 +0800

    a

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -34,28 +34,10 @@
 		<div class="topbar-info" id="J_userInfo">
 			@if (session('user'))
 			<div class="topbar-info" id="J_userInfo">
-				<span class="user">
-					<a rel="nofollow" class="user-name" href="/user" target="_blank" data-stat-id="fa66db4fed0eb581" onclick="_msq.push(['trackEvent', '79fe2eae924d2a2e-fa66db4fed0eb581', '//my.mi.com/portal', 'pcpid']);">
-					<span class="name" id="username"><?php session_start(); echo $_SESSION['user']; ?></span>
-					<i class="iconfont"></i></a>
-					<ul class="user-menu" id='userul'>
-						<li>
-							<a rel="nofollow" href="/user" target="_blank" data-stat-id="e0b9e1d1fa8052a2" onclick="_msq.push(['trackEvent', '79fe2eae924d2a2e-e0b9e1d1fa8052a2', '//my.mi.com/portal', 'pcpid']);">个人中心</a>
-						</li>
-						<li>
-							<a rel="nofollow" href="//order.mi.com/user/comment" target="_blank" data-stat-id="6d05445058873c2c" onclick="_msq.push(['trackEvent', '79fe2eae924d2a2e-6d05445058873c2c', '//order.mi.com/user/comment', 'pcpid']);">评价晒单</a>
-						</li>
-						<li>
-							<a rel="nofollow" href="//order.mi.com/user/favorite" target="_blank" data-stat-id="32e2967e9a749d3d" onclick="_msq.push(['trackEvent', '79fe2eae924d2a2e-32e2967e9a749d3d', '//order.mi.com/user/favorite', 'pcpid']);">我的喜欢</a>
-						</li>
-						<li>
-							<a rel="nofollow" href="http://account.xiaomi.com/" target="_blank" data-stat-id="6c2aba14bc7f649a" onclick="_msq.push(['trackEvent', '79fe2eae924d2a2e-6c2aba14bc7f649a', 'http://account.xiaomi.com/', 'pcpid']);">小米账户</a>
-						</li>
-						<li>
-							<a rel="nofollow" href="//order.mi.com/site/logout" data-stat-id="770a31519c713b11" onclick="_msq.push(['trackEvent', '79fe2eae924d2a2e-770a31519c713b11', '//order.mi.com/site/logout', 'pcpid']);">退出登录</a>
-						</li>
-					</ul>
-				</span>
+				
+
+				<span class="user"><a rel="nofollow" class="user-name" href="//my.mi.com/portal" target="_blank" data-stat-id="5d33825e5b1cea4b" onclick="_msq.push(['trackEvent', '79fe2eae924d2a2e-5d33825e5b1cea4b', '//my.mi.com/portal', 'pcpid']);"><span class="name"><?php session_start(); echo $_SESSION['user']; ?></span><i class="iconfont"></i></a><ul class="user-menu" style="display: none;"><li><a rel="nofollow" href="//my.mi.com/portal" target="_blank" data-stat-id="e0b9e1d1fa8052a2" onclick="_msq.push(['trackEvent', '79fe2eae924d2a2e-e0b9e1d1fa8052a2', '//my.mi.com/portal', 'pcpid']);">个人中心</a></li><li><a rel="nofollow" href="//order.mi.com/user/comment" target="_blank" data-stat-id="6d05445058873c2c" onclick="_msq.push(['trackEvent', '79fe2eae924d2a2e-6d05445058873c2c', '//order.mi.com/user/comment', 'pcpid']);">评价晒单</a></li><li><a rel="nofollow" href="//order.mi.com/user/favorite" target="_blank" data-stat-id="32e2967e9a749d3d" onclick="_msq.push(['trackEvent', '79fe2eae924d2a2e-32e2967e9a749d3d', '//order.mi.com/user/favorite', 'pcpid']);">我的喜欢</a></li><li><a rel="nofollow" href="http://account.xiaomi.com/" target="_blank" data-stat-id="6c2aba14bc7f649a" onclick="_msq.push(['trackEvent', '79fe2eae924d2a2e-6c2aba14bc7f649a', 'http://account.xiaomi.com/', 'pcpid']);">小米账户</a></li><li><a rel="nofollow" href="//order.mi.com/site/logout" data-stat-id="770a31519c713b11" onclick="_msq.push(['trackEvent', '79fe2eae924d2a2e-770a31519c713b11', '//order.mi.com/site/logout', 'pcpid']);">退出登录</a></li></ul></span>
+
 				<span class="sep">|</span>
 				<span class="message">
 					<a rel="nofollow" href="/message" data-stat-id="7324b7edba019c56" target="_blank" onclick="_msq.push(['trackEvent', '79fe2eae924d2a2e-7324b7edba019c56', '//order.mi.com/message/list', 'pcpid']);">消息通知<i class="J_miMessageTotal"></i></a>
@@ -280,14 +262,14 @@
 		})();
 	</script>
 	<script type="text/javascript">
-		var username = document.getElementById('username')
-		var ul = document.getElementById('userul');
-		username.onmouseover = function(){
-			ul.style.display = '';
-		}
-		username.onmouseout = function(){
-			ul.style.display = 'none';
-		}
+		// var username = document.getElementById('username')
+		// var ul = document.getElementById('userul');
+		// username.onmouseover = function(){
+		// 	ul.style.display = '';
+		// }
+		// username.onmouseout = function(){
+		// 	ul.style.display = 'none';
+		// }
 	</script>
 </body>
 </html>
