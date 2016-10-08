9eda0b116ca49f5044fdaefc13ae0a56dc3132dd
xiaoxin <18034586614@163.com>
2016-10-08 17:30:47 +0800

    1

---
+++ app/Http/Controllers/home/UserController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers\home;
+
+use Illuminate\Http\Request;
+use App\Models\Goods;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    public function index()
+    {
+        $goods = new Goods();
+        $list = $goods->getType();
+        $data = $goods->getAll();
+        return view('home.user.userCenter')->with(['list'=>$list])->with(["data"=>$data]);
+    }
+
+    public function myOrder()
+    {
+        $goods = new Goods();
+        $list = $goods->getType();
+        $data = $goods->getAll();
+        return view('home.user.myOrder')->with(['list'=>$list])->with(["data"=>$data]);
+    }
+
+    public function message()
+    {
+        $goods = new Goods();
+        $list = $goods->getType();
+        $data = $goods->getAll();
+        return view('home.user.message')->with(['list'=>$list])->with(["data"=>$data]);
+    }
+
+    public function showOrder()
+    {
+        $goods = new Goods();
+        $list = $goods->getType();
+        $data = $goods->getAll();
+        return view('home.user.showOrder')->with(['list'=>$list])->with(["data"=>$data]);
+    }
+
+    public function like()
+    {
+        $goods = new Goods();
+        $list = $goods->getType();
+        $data = $goods->getAll();
+        return view('home.user.like')->with(['list'=>$list])->with(["data"=>$data]);
+    }
+
+    public function address()
+    {
+        $goods = new Goods();
+        $list = $goods->getType();
+        $data = $goods->getAll();
+        return view('home.user.address')->with(['list'=>$list])->with(["data"=>$data]);
+    }
+
+    public function server()
+    {
+        $goods = new Goods();
+        $list = $goods->getType();
+        $data = $goods->getAll();
+        return view('home.user.server')->with(['list'=>$list])->with(["data"=>$data]);
+    }
+
+}
